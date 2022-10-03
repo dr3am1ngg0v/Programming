3b8b6ac887a0b7de6b7f051f4a3ea89a7372cc56
Johannes Mitrofanov <113423156+dr3am1ngg0v@users.noreply.github.com>
2022-10-03 18:34:59 +0500

    Add files via upload

---
+++ Caesar Cypher.py
@@ -0,0 +1,77 @@
+# Работа выполнена студентом УрФУ Фт-210007 Митрофановым Иваном.
+# Данный код представляет собой шифратор сообщений методом шифра Цезаря.
+# В данном коде существует один недочёт, который описан в README.md на GitHub. Прошу с ним ознакомиться.
+
+
+import os
+os.system('cls')
+
+alphabet_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя1234567890'                   # Алфавит русского языка с цифрами
+alphabet_EN = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'                          # Алфавит английского языка с цифрами
+
+def choice_def():
+    choice = input('Зашифровать еще сообщение? (Да - 1 или Нет - 2): ')
+        
+    if choice == '1':
+        os.system('cls')
+        main()
+    else:
+        if choice == '2':
+            os.system('cls')
+            print('Удачного дня!')
+        else:
+            os.system('cls')
+            print("Произошла ошибка! Повторите ввод!")
+            choice_def()
+
+def main():
+    
+    message = input("Введите сообщение для шифровки: ").upper()          # Команда для ввода сообщения и его последующего преобразования в верхний регистр
+    
+    step = int(input("Укажите шаг смещения: "))                                                            # Шаг смещения
+    result = ''
+    
+    language = input("Укажите язык сообщения (RU или EN): ")              # Выбор языка сообщения
+
+    if language == 'RU' or language == 'ru':
+                                     # Алгоритм шифрования на русском языке
+        for i in message:
+                      
+            placement = alphabet_RU.find(i)
+            new_placement = placement + step
+                
+            if i in alphabet_RU:
+                result += alphabet_RU[new_placement % len(alphabet_RU)]
+            else:
+                result += i
+        
+        os.system('cls')
+        print("Шифрованное сообщение: ", result.capitalize())
+        choice_def()
+                
+    
+    else:
+                
+        if language == 'EN' or language == 'en':
+            
+            for i in message:                                               # Алгоритм шифрования на английском языке
+            
+                placement = alphabet_EN.find(i)
+                new_placement = placement + step
+                
+                if i in alphabet_EN:
+                    result += alphabet_EN[new_placement % len(alphabet_EN)]
+                else:
+                    result += i
+            
+            os.system('cls')
+            print("Шифрованное сообщение: ", result.capitalize())
+            choice_def()
+            
+        else:
+            os.system('cls')
+            print('Возникла ошибка при выборе языка!')
+            main()
+            
+main()
+
